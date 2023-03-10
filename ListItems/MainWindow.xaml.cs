ae9a7d6399ef027dfa9a042a364bc7b2f36702de
vbanex <olavbanex@gmail.com>
2023-03-10 08:48:55 +0100

    Add project files.

---
+++ ListItems/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListItems
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(txtName.Text) && !listBox.Items.Contains(txtName.Text))
+            {
+listBox.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input");
+                txtName.Clear();
+            }
+        }
+    }
+}
